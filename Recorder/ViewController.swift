825248ae049b44228893896ca44c8737878573db
Paco Gorina <fgorina@gmail.com>
2015-01-30 16:38:34 +0100

    Initial Commit

---
+++ Recorder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recorder
+//
+//  Created by Francisco Gorina Vanrell on 30/1/15.
+//  Copyright (c) 2015 Paco Gorina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
